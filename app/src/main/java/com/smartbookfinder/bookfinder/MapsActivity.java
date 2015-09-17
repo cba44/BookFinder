e8ab569ecc4a2f411079bf0ee394048afba4c2fb
Chiran Agalawatte <cba_44@yahoo.com>
2015-09-17 06:51:21 +0530

    Changed last known location

--- app/src/main/java/com/smartbookfinder/bookfinder/MapsActivity.java
+++ app/src/main/java/com/smartbookfinder/bookfinder/MapsActivity.java
@@ -2,8 +2,9 @@ package com.smartbookfinder.bookfinder;
 
 import android.content.Context;
 import android.content.Intent;
-import android.location.Criteria;
+//import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
@@ -15,6 +16,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    Location start = MainActivity.l;
+
+    double latitude = start.getLatitude();
+    double longitude = start.getLongitude();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,30 +76,61 @@ public class MapsActivity extends FragmentActivity {
         LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
         // Create a criteria object to retrieve provider
-        Criteria criteria = new Criteria();
+        //Criteria criteria = new Criteria();
+
+
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
 
         // Get the name of the best provider
-        String provider = locationManager.getBestProvider(criteria, true);
+        //String provider = locationManager.getBestProvider(criteria, true);
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
 
         // Get Current Location
-        Location myLocation = locationManager.getLastKnownLocation(provider);
+        //Location myLocation = locationManager.getLastKnownLocation(provider);
+        //Location myLocation = locationManager.;
 
         // set map type
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         // Get latitude of the current location
-        double latitude = myLocation.getLatitude();
+        //double latitude = myLocation.getLatitude();
 
         // Get longitude of the current location
-        double longitude = myLocation.getLongitude();
+        //double longitude = myLocation.getLongitude();
 
         Location destination = MainActivity.dest;
+
+
+
 
         double latitude1 = destination.getLatitude();
         double longitude1 = destination.getLongitude();
 
         String uri = "http://maps.google.com/maps?saddr=" + latitude+","+longitude+"&daddr="+latitude1+","+longitude1;
-        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
         intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
         startActivity(intent);
 
